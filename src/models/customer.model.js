abbc2bac429047fe1c25bb612eebc749d1647e84
saranyellanki <saran.yellanki12@gmail.com>
2022-02-23 21:22:20 +0530

    [Saran] Added: schema for customer details

---
+++ src/models/customer.model.js
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+const customerDetailsSchema = new Schema ({
+  addressType: {
+    type: String,
+    required: true
+  },
+  fullAddress: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps: true
+})
+
+export default model ('CustomerDetails', customerDetailsSchema)
